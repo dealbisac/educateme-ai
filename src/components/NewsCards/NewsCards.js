0f8fb07865b27fd33e4cbde88a5521e9db0901ff
Dipendra Bahadur Chand <chand.dipendra19@gmail.com>
2020-08-29 19:38:40 +0545

    code refactor

--- src/components/NewsCards/NewsCards.js
+++ src/components/NewsCards/NewsCards.js
@@ -21,9 +21,9 @@ const NewsCards = ({ articles, activeArticle }) => {
           {infoCards.map((infoCard) => (
             <Grid item xs={12} sm={6} md={4} lg={3} className={classes.infoCard}>
               <div className={classes.card} style={{ backgroundColor: infoCard.color }}>
-                <Typography variant="h6" component="h6"><b><u>{infoCard.title}</u></b></Typography>
+                <Typography variant="p" component="p"><b><u>{infoCard.title}</u></b></Typography>
                 {infoCard.info ? <Typography variant="h6" component="h6"><strong>{infoCard.title.split(' ')[2]}</strong>: <br />{infoCard.info}</Typography> : null}
-                <Typography variant="h6" component="h6">Try saying: "<br /> <i>{infoCard.text}</i></Typography>
+                <Typography variant="p" component="p">Try saying: <br /> "<i>{infoCard.text}"</i></Typography>
               </div>
             </Grid>
           ))}
